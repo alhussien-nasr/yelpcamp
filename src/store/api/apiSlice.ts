f7850aeee67382049c70e3a90fbbb805adba0b54
hussein <alhussien.n.g@gmail.com>
2023-02-12 19:51:46 +0200

    edit baseUrl

--- src/store/api/apiSlice.ts
+++ src/store/api/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "campgroundsAPi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://yelpcamp-api.onrender.com" }),
   tagTypes: ["Campgrounds"],
   endpoints: () => ({}),
 });
